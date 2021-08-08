688acfc3c615562b2b89a99494d5446cc490e2a0
github-dns147 <rarden@ya.ru>
2021-08-08 17:51:56 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  const str = Math.abs(n).toString();
+  const len = str.length - 1;
+  let res = '';
+
+  for (i = len; i >= 0; i--) {
+      res += str[i];
+  }
+
+  return res;
 }
